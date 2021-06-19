dec80efcd4e0d971998c6fa9d8e51e92af317def
unknown <aakashsri.dev@gmail.com>
2021-06-19 15:21:48 +0530

    calling function only once using class

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 // import ClassCounter from "./components/ClassCounter";
 // import HookCounter from "./components/HookCounter";
 // import ClassInputCounter from "./components/ClassInputCounter";
-import HookInputCounter from "./components/HookInputCounter";
+// import HookInputCounter from "./components/HookInputCounter";
+import ClassMouse from './components/ClassMouse';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
       {/* <ClassInputCounter /> */}
-      <HookInputCounter />
+      <ClassMouse />
     </div>
   );
 }
