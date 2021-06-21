83f18d9b1847fb0912176d147ea91a96daecd174
unknown <aakashsri.dev@gmail.com>
2021-06-21 08:03:08 +0530

    automatic focus on input using useRef

---
+++ use-ref/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import FocusInput from './components/FocusInput'
+
+function App() {
+  return (
+    <FocusInput />
+  );
+}
+
+export default App;
