a38de802ae3017c6dcfbc070a1046884dbb40084
unknown <aakashsri.dev@gmail.com>
2021-07-08 19:00:45 +0530

    updated themeContextProvider class component with functional one

--- use-context/src/components/ThemeContext/themeContext.js
+++ use-context/src/components/ThemeContext/themeContext.js
@@ -1,26 +1,18 @@
-import React, {Component} from "react"
+import React, { Component, useState } from "react"
 const ThemeContext = React.createContext()
 
-class ThemeContextProvider extends Component {
-    state = {
-        theme: "dark"
+function ThemeContextProvider(props) {
+    const [theme, setTheme] = useState('dark')
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')
     }
-    
-    toggleTheme = () => {
-        this.setState(prevState => {
-            return {
-                theme: prevState.theme === "light" ? "dark" : "light"
-            }
-        })
-    }
-    
-    render() {
-        return (
-            <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        )
-    }
+
+    return (
+        <ThemeContext.Provider value={{ theme: theme, toggleTheme: toggleTheme }}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
 }
 
-export {ThemeContextProvider, ThemeContext}
+export { ThemeContextProvider, ThemeContext }
