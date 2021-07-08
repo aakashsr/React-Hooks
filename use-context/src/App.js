45b4b211909770c150ea7d4dc41ac7027c9d408b
unknown <aakashsri.dev@gmail.com>
2021-07-08 18:28:13 +0530

    added ThemeContext example with toggle theme functionality

--- use-context/src/App.js
+++ use-context/src/App.js
@@ -1,16 +1,21 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 import "./App.css";
-import ComponentA from "./components/ComponentA";
-import ComponentB from "./components/ComponentB";
-export const UserContext = React.createContext();
+// import ComponentA from "./components/UserContext/ComponentA";
+// import ComponentB from "./components/UserContext/ComponentB";
+import Landing from "./components/ThemeContext/Landing";
+import { ThemeContextProvider } from "./components/ThemeContext/themeContext";
+// export const UserContext = React.createContext();
 
 function App() {
-  const [user,setUser] = useState('aakashsr')
+  // const [user, setUser] = useState("aakashsr");
   return (
     <div className="App">
-      <UserContext.Provider value={{user,setUser}}>
+      {/* <UserContext.Provider value={{user,setUser}}>
         <ComponentA />
-      </UserContext.Provider>
+      </UserContext.Provider> */}
+      <ThemeContextProvider>
+        <Landing />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ use-context/src/components/ThemeContext/Landing.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Header from "./Header"
+import Button from "./Button"
+
+function Landing() {
+    return (
+        <div>
+            <Header />
+            <Button />
+        </div>
+    )
+}
+
+export default Landing
