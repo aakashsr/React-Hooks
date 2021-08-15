2ab27395e4fdac67a12d6f1355d709804d9845d6
unknown <aakashsri.dev@gmail.com>
2021-08-15 11:10:12 +0530

    Added Counter example using simple useReducer

---
+++ use-reducer/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter1 from "./components/Counter1";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter1 />
+    </div>
+  );
+}
+
+export default App;
